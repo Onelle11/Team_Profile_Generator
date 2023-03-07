c9608108be407574c2f7aebce8e5761154245595
Onelle11 <ofullente@gmail.com>
2023-03-06 23:12:12 -0500

    tried to do the javascript but failed

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const inquirer = require("inquirer");
 const fs = require('fs');
 
 console.log(' Welcome to Team Profile Generator ');
+
+const questions = [
+
+    {
+        type: 'input',
+        name: 'Employee',
+        message: 'What is the employee\'s name?',
+        validate: your_Input => {
+            if (your_Input) {
+                return true;
+            } else {
+                console.log('Enter the employee name');
+                return false;
+            }
+        }
+    }
+]
